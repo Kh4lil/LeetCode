eb15f96ef2363da868b00fa68911c8c3f0be4c61
Khalil <khalil.oumimoun@gmail.com>
2019-05-20 21:55:09 -0500

    Algo: #9

---
+++ LeetCode/PalindromeNumber.js
@@ -0,0 +1,42 @@
+/*
+Determine whether an integer is a palindrome. 
+An integer is a palindrome when it reads the same backward as forward.
+
+Example 1:
+
+Input: 121
+Output: true
+Example 2:
+
+Input: -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, 
+            it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: 10
+Output: false
+Explanation: Reads 01 from right to left. 
+                Therefore it is not a palindrome.
+*/
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+    let intToString = x.toString();
+    let splitString = intToString.split("");
+    let reversedString = splitString.reverse()
+
+    digits = reversedString.map(Number);
+    digits = digits.join('');
+    result = parseInt(digits);
+
+    if (result == x) {
+        return true;
+    } else {
+        return false;
+    }
+
+};
